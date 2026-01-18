130ca2354cd808903a098108590c86ebce11f666
Amadou <zhibet224@gmail.com>
2026-01-17 21:29:57 -0500

    Day 1 Creating database and Tables

---
+++ Day 1 Creating database and Tables/Creating Databases and Tables.sql
@@ -0,0 +1,28 @@
+-- Command to create the Database 
+	-- create database Bakery;
+
+-- Comand to use the database 
+	-- use Bakery;
+
+-- Command to see the current database being used
+	-- select database();
+
+-- Command to create a table in the database
+	-- create table cakes (
+	-- 	name varchar(200),
+	-- 	amount int
+	--   );
+
+-- create table ice_cream (
+	-- 	name varchar(200),
+	-- 	amount int
+	--   );
+  
+  
+-- create table cake_cakes (
+	-- 	name varchar(200),
+	-- 	amount int
+	--   );
+
+-- Drop a table in the database
+	-- drop table ice_cream;
