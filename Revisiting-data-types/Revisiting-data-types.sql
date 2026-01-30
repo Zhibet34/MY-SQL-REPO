b116673024dc77ed050e7c9b3d02fa4759176a1e
Amadou <zhibet224@gmail.com>
2026-01-29 19:02:27 -0500

    Add files via upload

---
+++ Revisiting-data-types/Revisiting-data-types.sql
@@ -0,0 +1,29 @@
+-- use bakery;
+
+-- select database();
+
+-- exercise
+
+-- create	table inventory(
+-- 	item_name varchar(100),
+--     price decimal(7,2),
+--     quantity int(100)
+-- );
+
+-- select curtime() as Time;
+
+-- select current_date() as Date;
+
+-- select dayofweek(current_date()) as 'Date Of The Week';
+
+-- select dayname(current_date()) as "Day name";
+
+-- select date_format(current_date(),'%m/%d/%Y') as DATE
+
+-- select date_format(now(),'%M %D at %k:%i') as 'Month format'
+
+-- create table Tweets_table (
+-- 	tweet varchar(150),
+--     username varchar(100),
+--     created_at timestamp default current_timestamp
+-- );
